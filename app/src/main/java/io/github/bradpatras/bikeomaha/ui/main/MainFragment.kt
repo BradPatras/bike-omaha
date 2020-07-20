8b68fb50e07d4227b61d8b652a0ba9667458df44
Brad <BradPatras@users.noreply.github.com>
2020-07-20 17:16:38 -0500

    setup mapview

--- app/src/main/java/io/github/bradpatras/bikeomaha/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/bradpatras/bikeomaha/ui/main/MainFragment.kt
@@ -1,30 +1,71 @@
 package io.github.bradpatras.bikeomaha.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import io.github.bradpatras.bikeomaha.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment), OnMapReadyCallback {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
+    private var map: GoogleMap? = null
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        map_view?.let {
+            it.onCreate(savedInstanceState)
+            it.getMapAsync(this)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map_view?.onResume()
+    }
+
+    override fun onDestroyView() {
+        map_view?.onDestroy()
+        super.onDestroyView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map_view?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map_view?.onLowMemory()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map_view?.onStart()
+    }
 }
