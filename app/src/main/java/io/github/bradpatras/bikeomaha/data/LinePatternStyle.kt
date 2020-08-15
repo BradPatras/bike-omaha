6cc737d8632c04913036b85054ae18f779a97a8e
Brad <BradPatras@users.noreply.github.com>
2020-08-15 14:53:00 -0500

    fixed pattern enum

--- app/src/main/java/io/github/bradpatras/bikeomaha/data/LinePatternStyle.kt
+++ app/src/main/java/io/github/bradpatras/bikeomaha/data/LinePatternStyle.kt
@@ -1,6 +1,5 @@
 package io.github.bradpatras.bikeomaha.data
 
-enum class LinePatternStyle(val stringVal: String) {
-    DOTTED("dotted"),
-    SOLID("solid")
+enum class LinePatternStyle {
+    DOTTED, SOLID
 }
