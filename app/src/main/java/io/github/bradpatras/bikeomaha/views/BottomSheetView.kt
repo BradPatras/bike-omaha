7c26b44bb1c0c9fe853478b64c5c5c2139c3755c
Brad <BradPatras@users.noreply.github.com>
2020-11-24 21:26:42 -0600

    styled the bottom sheet and put it into its own class

---
+++ app/src/main/java/io/github/bradpatras/bikeomaha/views/BottomSheetView.kt
@@ -0,0 +1,31 @@
+package io.github.bradpatras.bikeomaha.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import io.github.bradpatras.bikeomaha.R
+
+class BottomSheetView : ConstraintLayout {
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        inflate(context, R.layout.bottom_sheet_view, this)
+    }
+}
